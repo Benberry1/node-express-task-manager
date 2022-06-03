2092b754e4329771abd33a246776c1df42ef29db
Ben Berry <ben.berry@developersinstitute.co.nz>
2022-06-03 14:33:57 +1200

    add GET /tasks and GET /tasks/:id endpoint

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,13 @@
 const TaskModel = require("../models/TaskModel");
+const mongoose = require("mongoose");
 
-const getAllTasks = (req, res) => {
-  res.send({ body: "get all tasks" });
+const getAllTasks = async (req, res) => {
+  try {
+    const allTasks = await TaskModel.find({});
+    res.status(200).send(allTasks);
+  } catch (error) {
+    res.status(500).send({ message: "There was an error" });
+  }
 };
 
 const createTask = async (req, res) => {
@@ -15,8 +21,25 @@ const createTask = async (req, res) => {
   }
 };
 
-const getSingleTask = (req, res) => {
-  res.send({ body: req.params.id });
+const getSingleTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const isValidId = mongoose.Types.ObjectId.isValid(id);
+    console.log(isValidId);
+    if (isValidId) {
+      const task = await TaskModel.findById({ _id: id });
+      if (task) {
+        return res.status(200).send(task);
+      } else {
+        return res.status(404).send({ message: "task not found" });
+      }
+    } else {
+      res.status(400).send({ message: "invalid id" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "There was an error" });
+  }
 };
 
 const updateTask = (req, res) => {
