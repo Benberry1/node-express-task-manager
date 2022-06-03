8bbf32466bda645cd6ed317f6eb2416c6e2234cb
Ben Berry <ben.berry@developersinstitute.co.nz>
2022-06-03 13:10:09 +1200

    correct mongoose schema
    remove required from completed value, add default: false

--- models/TaskModel.js
+++ models/TaskModel.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const taskSchema = new Schema(
   {
     name: { type: String, required: true },
-    completed: { type: Boolean, required: true },
+    completed: { type: Boolean, default: false },
   },
   {
     toJSON: {
